10ba66739469c9330a1db5616b452f8270cf6f12
homer0 <leo.apiwan@olapic.com>
2020-09-03 19:02:53 -0300

    fix: user lowercase port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get<ConfigService>('ConfigService');
-  const port = config.get<number>('app.PORT');
+  const port = config.get<number>('app.port');
   await app.listen(port);
   Logger.log(`Server running on port ${port}`, 'Bootstrap');
 }
