3588492e10f3d5dacd6f4a19879dfdc92eb83bd2
Agustin Carrasco <asermax@gmail.com>
2020-09-04 02:01:56 -0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = app.get(ConfigService);
   const port = config.get<number>('app.port');
   await app.listen(port);
